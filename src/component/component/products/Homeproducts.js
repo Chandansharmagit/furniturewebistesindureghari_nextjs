40c899835fe1d7624022d2d9ab8ce71378fb7789
Chandan Sharma <utamsharma57@gmail.com>
2026-05-19 16:43:49 +0545

    fixing seo bugs

--- src/component/component/products/Homeproducts.js
+++ src/component/component/products/Homeproducts.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import './hproduct.css';
 import { FaShoppingCart, FaHeart, FaSearch, FaInfoCircle, FaStar } from 'react-icons/fa';
--- src/component/productdetails/Productdetails.js
+++ src/component/productdetails/Productdetails.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
